c5b7a08911fc02bdeecf0fd36019069adf0d646a
RayHaha <g820324@gmail.com>
2019-08-15 21:49:24 -0400

    manage recipes in a recipe service

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit, EventEmitter, Output } from '@angular/core';
 
 import { Recipe } from '../recipe.model';
+import { RecipeService } from '../recipe.service';
 
 @Component({
   selector: 'app-recipe-list',
@@ -9,16 +10,18 @@ import { Recipe } from '../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
   @Output() recipeWasSelected = new EventEmitter<Recipe>();
-  recipes: Recipe[] = [
-    new Recipe('Test Recipe', 'This is a test', 
-    'https://ichef.bbci.co.uk/news/320/cpsprodpb/27C9/production/_103158101_tha.jpg'),
-    new Recipe('Another Recipe', 'This is a test', 
-    'https://ichef.bbci.co.uk/news/320/cpsprodpb/27C9/production/_103158101_tha.jpg')
-  ];
+  // recipes: Recipe[] = [
+  //   new Recipe('Test Recipe', 'This is a test', 
+  //   'https://ichef.bbci.co.uk/news/320/cpsprodpb/27C9/production/_103158101_tha.jpg'),
+  //   new Recipe('Another Recipe', 'This is a test', 
+  //   'https://ichef.bbci.co.uk/news/320/cpsprodpb/27C9/production/_103158101_tha.jpg')
+  // ];
+  recipes: Recipe[] = [];
 
-  constructor() { }
+  constructor(private recipeService: RecipeService) { }
 
   ngOnInit() {
+    this.recipes = this.recipeService.getRecipes();
   }
 
   onRecipeSelected(recipe: Recipe){
--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,3 +1,15 @@
+import { Recipe } from './recipe.model';
+
 export class RecipeService {
-    
+    private recipes: Recipe[] = [
+        new Recipe('Test Recipe', 'This is a test', 
+        'https://ichef.bbci.co.uk/news/320/cpsprodpb/27C9/production/_103158101_tha.jpg'),
+        new Recipe('Another Recipe', 'This is a test', 
+        'https://ichef.bbci.co.uk/news/320/cpsprodpb/27C9/production/_103158101_tha.jpg')
+      ];
+
+    getRecipes(){
+        return this.recipes.slice();
+    }
+
 }
