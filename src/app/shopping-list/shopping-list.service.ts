2f9469f31aec22966ee38ff0317f7ec4bf891743
RayHaha <g820324@gmail.com>
2019-08-17 17:21:18 -0400

    add the shoppinglist service

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,3 +1,16 @@
+import { Ingredient } from '../shared/ingredient.model';
+
 export class ShoppingListService {
-    
+    private ingredients: Ingredient[] = [
+        new Ingredient('Apples', 3),
+        new Ingredient('Tomatoes', 6)
+      ];
+
+    getIngredients(){
+        return this.ingredients.slice();
+    }
+
+    addIngredient(ingredient: Ingredient){
+        this.ingredients.push(ingredient);
+    }
 }
