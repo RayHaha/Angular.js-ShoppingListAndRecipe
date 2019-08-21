feba74978dbb40e03d735252e20357d490b6d2bc
RayHaha <g820324@gmail.com>
2019-08-20 20:29:24 -0400

    add ingredients to recipes

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -1,5 +1,6 @@
 import { Recipe } from './recipe.model';
 import { EventEmitter } from '@angular/core';
+import { Ingredient } from '../shared/ingredient.model';
 
 export class RecipeService {
     recipeSelected = new EventEmitter<Recipe>();
@@ -7,9 +8,17 @@ export class RecipeService {
 
     private recipes: Recipe[] = [
         new Recipe('Test Recipe', 'This is a test', 
-        'https://ichef.bbci.co.uk/news/320/cpsprodpb/27C9/production/_103158101_tha.jpg'),
+        'https://ichef.bbci.co.uk/news/320/cpsprodpb/27C9/production/_103158101_tha.jpg',
+        [
+            new Ingredient('Meat', 1),
+            new Ingredient('French Fries', 20)
+        ]),
         new Recipe('Another Recipe', 'This is a test', 
-        'https://ichef.bbci.co.uk/news/320/cpsprodpb/27C9/production/_103158101_tha.jpg')
+        'https://ichef.bbci.co.uk/news/320/cpsprodpb/27C9/production/_103158101_tha.jpg',
+        [
+            new Ingredient('rice', 1),
+            new Ingredient('chicken', 4)
+        ])
       ];
 
     getRecipes(){
