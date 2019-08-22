a13cc9abf147c231a5f66a216a04c932f5e7e372
RayHaha <g820324@gmail.com>
2019-08-21 21:22:22 -0400

    recipe service

--- src/app/recipes/recipe.service.ts
+++ src/app/recipes/recipe.service.ts
@@ -7,21 +7,21 @@ export class RecipeService {
 
 
     private recipes: Recipe[] = [
-        new Recipe('Test Recipe', 'This is a test', 
-        'https://ichef.bbci.co.uk/news/320/cpsprodpb/27C9/production/_103158101_tha.jpg',
-        [
-            new Ingredient('Meat', 1),
-            new Ingredient('French Fries', 20)
-        ]),
-        new Recipe('Another Recipe', 'This is a test', 
-        'https://ichef.bbci.co.uk/news/320/cpsprodpb/27C9/production/_103158101_tha.jpg',
-        [
-            new Ingredient('rice', 1),
-            new Ingredient('chicken', 4)
-        ])
-      ];
+        new Recipe('Test Recipe', 'This is a test',
+            'https://ichef.bbci.co.uk/news/320/cpsprodpb/27C9/production/_103158101_tha.jpg',
+            [
+                new Ingredient('Meat', 1),
+                new Ingredient('French Fries', 20)
+            ]),
+        new Recipe('Another Recipe', 'This is a test',
+            'https://ichef.bbci.co.uk/news/320/cpsprodpb/27C9/production/_103158101_tha.jpg',
+            [
+                new Ingredient('rice', 1),
+                new Ingredient('chicken', 4)
+            ])
+    ];
 
-    getRecipes(){
+    getRecipes() {
         return this.recipes.slice();
     }
 
