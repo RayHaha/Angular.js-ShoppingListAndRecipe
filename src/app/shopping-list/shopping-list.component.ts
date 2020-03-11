74c00dcacbf40116131907829d59cab3491bff0b
RayHaha <g820324@gmail.com>
2020-03-11 15:34:47 -0400

    change the subscription name

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -15,20 +15,20 @@ export class ShoppingListComponent implements OnInit, OnDestroy {
   //   new Ingredient('Tomatoes', 6)
   // ];
   ingredients: Ingredient[] = [];
-  private igChangeSub: Subscription;
+  private subscription: Subscription;
 
   constructor(private slService: ShoppingListService) { }
 
   ngOnInit() {
     this.ingredients = this.slService.getIngredients();
-    this.igChangeSub = this.slService.ingredientsChanged.subscribe(
+    this.subscription = this.slService.ingredientsChanged.subscribe(
       (ingredients: Ingredient[]) => {
         this.ingredients = ingredients;
       }
     );
   }
   ngOnDestroy(): void {
-    this.igChangeSub.unsubscribe();
+    this.subscription.unsubscribe();
   }
 
   // onIngredientAdded(ingredient: Ingredient){
