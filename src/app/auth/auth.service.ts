87d4b3837eb0828dac79e5077a0455dbe2277b0d
RayHaha <g820324@gmail.com>
2020-06-05 00:17:50 -0400

    send the signup request

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -18,7 +18,9 @@ export class AuthService {
     }
     
     signup(email: string, password: string){
-        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signInWithCustomToken?key=AIzaSyDcMFC4JAx2TW6e9HTWTi6EUGwj-6xWKQY',{
+        console.log(email, password);
+        return this.http.post<AuthResponseData>('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyDcMFC4JAx2TW6e9HTWTi6EUGwj-6xWKQY',
+        {
             email: email,
             password: password,
             returnSecureToken: true
