ac5207c121f31c43022ffe34d9d0c827f8c75e6b
RayHaha <g820324@gmail.com>
2020-06-22 22:12:42 -0400

    reflect the auth state in the UI

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,26 +1,36 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component, EventEmitter, Output, OnInit, OnDestroy } from '@angular/core';
 import { DataStorageService } from '../shared/data-storage.service';
+import { AuthService } from '../auth/auth.service';
+import { Subscription } from 'rxjs';
 
 @Component({
     selector: 'app-header',
     templateUrl: './header.component.html'
 })
 
-export class HeaderComponent {
+export class HeaderComponent implements OnInit, OnDestroy {
     // @Output() featureSelected = new EventEmitter<string>();
 
     // onSelect(feature: string){
     //     this.featureSelected.emit(feature);
     // }
-
-    constructor(private dataStorageService: DataStorageService){
+    isAuthenticated = false;
+    private userSub: Subscription;
+    constructor(private dataStorageService: DataStorageService, private authService: AuthService){
 
     }
-
+    ngOnInit(){
+        this.userSub = this.authService.user.subscribe(user => {
+            // this.isAuthenticated = !user ? false: true; can shorten to
+            this.isAuthenticated = !!user;
+        });
+    }
+    ngOnDestroy(){
+        this.userSub.unsubscribe();
+    }
     onSaveData(){
         this.dataStorageService.storeRecipes();
     }
-
     onFetchData(){
         this.dataStorageService.fetchRecipes().subscribe();
     }
