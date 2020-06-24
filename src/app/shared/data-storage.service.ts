3829426cafebfc20d739113140241d7a0a9124b5
RayHaha <g820324@gmail.com>
2020-06-24 00:46:15 -0400

    data storage service

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -19,6 +19,7 @@ export class DataStorageService{
             console.log(response);
         });
     }
+    
     fetchRecipes(){
         return this.authService.user.pipe(
             take(1),
