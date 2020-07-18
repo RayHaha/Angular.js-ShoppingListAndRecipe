04ecd8a7f6e3836a3e7479488310dd5ca5414316
RayHaha <g820324@gmail.com>
2020-07-18 15:07:12 -0400

    attach the token with an interceptor

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -22,19 +22,14 @@ export class DataStorageService{
     }
     
     fetchRecipes(){
-        return this.authService.user.pipe(
-            take(1),
-            exhaustMap(user => {
-                return this.http.get<Recipe[]>('https://ng-course-recipe-book-305a9.firebaseio.com/recipes.json', {
-                    params: new HttpParams().set('auth', user.token)
-                });
-            }),map(recipes => {
+        return this.http.get<Recipe[]>('https://ng-course-recipe-book-305a9.firebaseio.com/recipes.json').pipe(
+            map(recipes => {
                 return recipes.map(recipe => {
                     return {...recipe, ingredients: recipe.ingredients ? recipe.ingredients : []};
                 });
             }),tap(recipes => {
             this.recipeService.setRecipes(recipes);
             })
-        );
+        )
     }
 }
