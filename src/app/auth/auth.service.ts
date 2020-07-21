4b39ca2dfb243b6a5748ecd2ec06809a714881d0
RayHaha <g820324@gmail.com>
2020-07-20 20:59:03 -0400

    add auto-login

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -45,6 +45,22 @@ export class AuthService {
             // resData.expiresIn is converted to a number by a an extra plus in front of it
         }));
     }
+    autoLogin(){
+        const userData: {
+            email: string;
+            id: string;
+            _token: string;
+            _tokenExpirationDate: string;
+        } = JSON.parse(localStorage.getItem('userData'));
+        if(!userData){
+            return;
+        }
+        const loadedUser = new User(userData.email, userData.id, userData._token, new Date(userData._tokenExpirationDate));
+
+        if(loadedUser.token){
+            this.user.next(loadedUser);
+        }
+    }
     logout(){
         this.user.next(null);
         this.router.navigate(['/auth']);
@@ -55,6 +71,7 @@ export class AuthService {
             // then use new Date to turn back to Date object
             const user = new User(email, userId, token, expirationDate);
             this.user.next(user);
+            localStorage.setItem('userData', JSON.stringify(user));
     }
     private handleError(errorRes: HttpErrorResponse){
         let errorMessage = 'An unknown error occurred!';
