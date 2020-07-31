3c2bbc64e43dec03c47a035a1e85a8ae2ba7ea22
RayHaha <g820324@gmail.com>
2020-07-31 00:58:39 -0400

    split module correctly

--- src/app/recipes/recipes.module.ts
+++ src/app/recipes/recipes.module.ts
@@ -5,6 +5,9 @@ import { RecipeDetailComponent } from './recipe-detail/recipe-detail.component';
 import { RecipeItemComponent } from './recipe-list/recipe-item/recipe-item.component';
 import { RecipeStartComponent } from './recipe-start/recipe-start.component';
 import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     declarations: [
@@ -14,7 +17,10 @@ import { RecipeEditComponent } from './recipe-edit/recipe-edit.component';
         RecipeItemComponent,
         RecipeStartComponent,
         RecipeEditComponent
-    ],
+    ],/** BrowserModule must only to be used once in appmodule. It does more than just ngIf and ngFor
+    it does some general application start up work that only has to run once.
+    Instead of BrowserModule, use common module in all other place where you want to get access to ngIf and ngFor  */
+    imports: [RouterModule, CommonModule, ReactiveFormsModule],
     exports: [
         RecipesComponent,
         RecipeListComponent,
